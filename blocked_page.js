318cd406569c559e746c540aad922bbaedaa7dec
Claire Kolln <ckolln55@gmail.com>
2020-03-08 10:24:43 -0700

    Add time in local storage when user decides to stay on the page. Requires reason before redirecting.
    Adjusts local storage timeLeft, hoursLeft, and minutesLeft values

--- blocked_page.js
+++ blocked_page.js
@@ -47,64 +47,110 @@ function onError(error) {
  * 
  * Args: None
  * 
- * Returns: String 
+ * Returns: None 
  */ 
 
 function createInput() {
-    var reason = "REASON";
-    var reasons_input = document.getElementById("buttons");
-
+    
+    var reason = "";
+    var reasons_input = document.getElementById("reason");
+    
+    // add the select element, replacing the button 
     reasons_input.innerHTML = '<div id="select"><select id="slct" name="reason_input"><option selected="selected">Please select a reason...</option> <option value="Task was work related">This is work related</option><option value="Almost done with task">I am almost done</option><option value="Had no work that needed focus">I have nothing that needs focus</option><option value="Other">Other</option> </select><div>'
     var select_field = document.getElementById("slct");
+   
+    // add a submit and redirect button
     document.getElementById("submit_button").innerHTML = '<button class="btn" id="submit">Submit and Redirect</button>'
     
+    // listen for change to the select element, assign reason variable
     select_field.addEventListener('change', function() {
         reason = select_field.options[select_field.selectedIndex].value;
-        // console.log(reason);
-        // console.log(reason);
-
+        
+        // if other is selected, add a text box for user input and remove if other is unselected
         if (reason == "Other") {
             console.log("Other selected");
             document.getElementById("other").innerHTML = '<input type="text" id="other_reason" value="Enter other reason..."> </input>';
-            // reason = document.getElementById("other_reason").value;
-            // console.log(reason);
         }
         else {
             document.getElementById("other").innerHTML = '';
         }
     });
 
+    // listen for submit button click
     document.getElementById("submit").addEventListener('click', function() {
         console.log("Clicked");
-        var is_other = 0;
+        
         if (reason == "Other") {
             reason =  document.getElementById("other_reason").value;
-            is_other = 1;
+            save_reason(reason);
         }
-
-        save_reason(reason,is_other);
+        else if (reason == "") {
+            document.getElementById("message").innerHTML = "<p>Please enter a reason</p>";
+        }
+        else {
+            save_reason(reason);
+        }
     });
 
-    /* 
-    * Stores the reasons in local storage
-    */
-
-   function redirectToBlocked() {
+    /*
+    * Gets the blockedSite from local storage, and redirects
+    * 
+    * Author: Claire Kolln
+    * 
+    * Args: None
+    * 
+    * Returns: None 
+    */ 
+    function redirectToBlocked() {
         let site = browser.storage.local.get("blockedSite");
         site.then(onGot,onError);
     }
 
+    /*
+    * Tests to make sure values are being stored as expected
+    * 
+    * Author: Claire Kolln
+    * 
+    * Args: None
+    * 
+    * Returns: None 
+    */ 
     function testStorage() {
         let gettingReasons = browser.storage.local.get("reasons");
         gettingReasons.then(onStorage,onError);
     }
 
-    function save_reason(reason,other) {
+    /*
+    * Adds 15 minutes to user's timeleft in local storage when they decide to stay on distracting site
+    * 
+    * Author: Claire Kolln
+    * 
+    * Args: None
+    * 
+    * Returns: None 
+    */ 
+   function addTime() {
+        browser.storage.local.set({"time_left": 900000});
+        browser.storage.local.set({"hoursLeft": 0});
+        browser.storage.local.set({"minutesLeft": 900000});
+    }
+
+    /*
+    * Saves reason in local storage
+    * 
+    * Author: Claire Kolln
+    * 
+    * Args: String (reason)
+    * 
+    * Returns: None 
+    */ 
+    function save_reason(reason) {
         
         console.log("Submitting reason " + reason);
         var gettingReasons = browser.storage.local.get();
         
         gettingReasons.then(results => {
+           
             // if there are no reasons stored, initialize the object
             console.log("Getting reasons");
             if (!results.reasons) {
@@ -117,12 +163,19 @@ function createInput() {
         
             browser.storage.local.set({reasons: results.reasons});
         })
+
+        addTime();
         
-        testStorage();
+        // testStorage();
         redirectToBlocked();
     }
-
 }
+
+/*
+* Listens for "stay" button click and then calls createInput()
+* 
+* Author: Claire Kolln
+*/ 
 
 document.getElementById("stay").addEventListener('click', function() {
     createInput();
