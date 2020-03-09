b61da033d17ff74e9b8e98016ac8a036d1b5b0cb
Claire Kolln <ckolln55@gmail.com>
2020-03-08 23:39:39 -0700

    Finished Stats page
    Added Chart.js external library to make pie chart dynamically

--- blocked_page.js
+++ blocked_page.js
@@ -56,7 +56,7 @@ function createInput() {
     var reasons_input = document.getElementById("reason");
     
     // add the select element, replacing the button 
-    reasons_input.innerHTML = '<div id="select"><select id="slct" name="reason_input"><option selected="selected">Please select a reason...</option> <option value="Task was work related">This is work related</option><option value="Almost done with task">I am almost done</option><option value="Had no work that needed focus">I have nothing that needs focus</option><option value="Other">Other</option> </select><div>'
+    reasons_input.innerHTML = '<div id="select"><select id="slct" name="reason_input"><option selected="selected">Please select a reason...</option> <option value="Task was work related">This is work related</option><option value="Was almost done with task">I am almost done</option><option value="Had no work that needed focus">I have nothing that needs focus</option><option value="Other">Other</option> </select><div>'
     var select_field = document.getElementById("slct");
    
     // add a submit and redirect button
@@ -79,16 +79,19 @@ function createInput() {
     // listen for submit button click
     document.getElementById("submit").addEventListener('click', function() {
         console.log("Clicked");
+        var is_other = 0;
         
         if (reason == "Other") {
+            console.log("OTHER");
             reason =  document.getElementById("other_reason").value;
-            save_reason(reason);
+            is_other = 1;
+            save_reason(reason,is_other);
         }
         else if (reason == "") {
             document.getElementById("message").innerHTML = "<p>Please enter a reason</p>";
         }
         else {
-            save_reason(reason);
+            save_reason(reason, is_other);
         }
     });
 
@@ -144,30 +147,44 @@ function createInput() {
     * 
     * Returns: None 
     */ 
-    function save_reason(reason) {
+
+    function save_reason(reason, is_other) {
         
         console.log("Submitting reason " + reason);
         var gettingReasons = browser.storage.local.get();
         
         gettingReasons.then(results => {
-           
+
             // if there are no reasons stored, initialize the object
             console.log("Getting reasons");
+            results.reasons = {
+                    default: {},
+                    other: []
+                };
             if (!results.reasons) {
                 console.log("There are no saved reasons, initializing");
-                results.reasons = {};
+                results.reasons = {
+                    default: {},
+                    other: []
+                };
             }
 
-            results.reasons[reason] = results.reasons[reason] || 0;
-            results.reasons[reason]++;
-        
+            if (!is_other) {
+                results.reasons.default[reason] = results.reasons.default[reason] || 0;
+                results.reasons.default[reason]++;
+            }
+            else {
+                results.reasons.other.push(reason);
+            }
+
             browser.storage.local.set({reasons: results.reasons});
+
         })
 
         addTime();
         
-        // testStorage();
-        redirectToBlocked();
+        testStorage();
+        // redirectToBlocked();
     }
 }
 
---
+++ widget/stats.js
@@ -0,0 +1,183 @@
+/* 
+ * Call the topSites API to get pages that the user has visited frequently. Display on stats.html
+ * 
+ * Author: Claire Kolln
+ * 
+ * Args: None
+ * 
+ * Returns: None
+ */ 
+
+function getTopSites() {
+    var top_div = document.getElementById("top");
+    var gettingTopSites = browser.topSites.get({
+        onePerDomain: true,
+        limit: 10,
+        includeFavicon: true
+    });
+
+    gettingTopSites.then(top => {
+        console.log(top);
+        for (var x = 0; x < top.length ; x++) {
+            var new_div = document.createElement("DIV");
+            new_div.classList.add("topsites");
+            
+            // get the favicon for the site
+            var node = document.createElement("IMG");
+            node.classList.add("icons");
+            node.display = "absolute";
+           
+            if (top[x].favicon == null) {
+                node.src = '../icons/anonymous.png';
+            }
+            else {
+                node.src = top[x].favicon;
+            }
+
+            // get the title
+            var span = document.createElement("P");
+            span.classList.add("titles");
+
+            // if no title, use url
+            if (top[x].title == "") {
+                var textnode = document.createTextNode(top[x].url);
+            }
+            else {
+                var textnode = document.createTextNode(top[x].title);         
+            }
+
+            span.appendChild(textnode);
+            new_div.appendChild(node);
+            new_div.appendChild(span);
+            
+            top_div.appendChild(new_div);
+        }
+    })
+}
+
+
+
+/*
+ * Use data to render pie chart
+ * 
+ * Author: Claire Kolln
+ * 
+ * Args: two data arrays
+ * 
+ * Returns: None
+ */ 
+
+function renderChart(data,labels) {
+    var ctx = document.getElementById('reasonChart').getContext('2d');
+    // console.log("made ctx");
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'pie',
+
+        // The data for our dataset
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    '#45281b',
+                    '#64462d',
+                    '#da7559',
+                    '#8fb48c',
+                    '#e1bf64'
+                ]
+            }],
+        },
+        options: {
+            legend: {
+                position: 'bottom'
+            },
+            title: {
+                display: true,
+                position: 'top',
+                fontFamily: "'Lucida Console', 'Monaco', 'monospace'",
+                text: "Your reasons for staying distracted",
+                padding: 15,
+                fontColor: 'black',
+                fontSize: 18
+            }
+        }
+    });
+}
+
+/*
+ * Dynamically list the "other" reasons
+ * 
+ * Author: Claire Kolln
+ * 
+ * Args: list
+ * 
+ * Returns: None
+ */ 
+function listOther(other) {
+    // get the other reasons from the list
+    // Add them, if there are none -> display that
+    console.log(other);
+    var list = document.getElementById("other_list");
+    var node = document.createElement("H3");                
+    var textnode = document.createTextNode("Other Reasons you stayed distracted:");         
+    node.appendChild(textnode);                              
+    document.getElementById("other").insertBefore(node,list); 
+
+    if (other.length == 0) {
+        var node = document.createElement("LI");                 
+        var textnode = document.createTextNode("There is no data to display!");         
+        node.appendChild(textnode);                              
+        list.appendChild(node); 
+    }
+
+    for (var i = 0; i < other.length ; i++) {
+        var node = document.createElement("LI");                 
+        var textnode = document.createTextNode(other[i]);         
+        node.appendChild(textnode);                              
+        list.appendChild(node); 
+    }
+}
+
+/*
+ * Get data from local storage and pass to functions above
+ * 
+ * Author: Claire Kolln
+ * 
+ * Args: None
+ * 
+ * Returns: None
+ */ 
+function getData() {
+
+    let gettingReasons = browser.storage.local.get("reasons");
+    gettingReasons.then(results => {
+
+        // if there are no reasons stored, display that
+        console.log("Getting reasons");
+        if (!results.reasons) {
+            document.getElementById("pie-chart").innerHTML = '<p> Yay! You have not gone past your time limit </p>';
+            // display that there are no reasons so display
+            console.log("There are no saved reasons -> user hasn't chosen to stay distracted");
+        }
+        else {
+
+            // get all the non-other justifications
+            var data = Object.values(results.reasons.default);
+            var labels = Object.keys(results.reasons.default);
+            
+            // add the other count
+            data.push(Object.keys(results.reasons.other).length);
+            labels.push("Other");
+
+            // console.log(data.datasets);
+            renderChart(data,labels);
+            listOther(results.reasons.other);
+            getTopSites();
+        }
+    })
+}
+
+
+// MAIN CALL ON PAGE LOAD
+getData();
